26d8fc8071b7b90bcce88f48a6005c79173523aa
andrew robertson <andrewcrobertson@gmail.com>
2025-01-30 19:12:04 +1100

    bump-deps

---
+++ .codegen/config.ts
@@ -0,0 +1,46 @@
+export const infisical = {
+  projectSlug: 'execution-environments-v5w-q',
+  secretPath: '/github'
+}
+
+export const npm = {
+  registry: 'https://npm.pkg.github.com/',
+  tokenName: 'NPM_TOKEN',
+  scopes: ['studio-75', 'ismyclub'],
+}
+
+export const website = {
+  name: 'Website',
+  package: '@ismyclub/app.website',
+  command: 'website',
+}
+
+export const cms = {
+  name: 'CMS',
+  package: '@ismyclub/app.cms',
+  command: 'cms',
+}
+
+export const fairpark = {
+  name: 'Fairpark',
+  package: '@ismyclub/app.fairpark',
+  command: 'fairpark',
+}
+
+export const signUp = {
+  name: 'Sign Up',
+  package: '@ismyclub/app.sign-up',
+  command: 'sign-up',
+}
+
+export const standardConfig = {
+  LOG_LEVEL: 'info',
+};
+
+export const pgConfig = {
+  PG_DATABASE: '',
+  PG_HOST: '',
+  PG_USER: '',
+  PG_PASSWORD: '',
+  PG_PORT: '',
+};
--- .codegen/index.ts
+++ .codegen/index.ts
@@ -1,52 +1,6 @@
 import { appConfig, dockerNodeApp, dockerNodeAppConfig, infisicalConfig, npmConfig } from '@studio-75/sdk.container';
 import { onboard } from '@studio-75/sdk.onboard';
-
-const infisical = {
-  projectSlug: 'main-qx-mr',
-  secretPath: '/ismyclub'
-}
-
-const npm = {
-  registry: 'https://npm.pkg.github.com/',
-  tokenName: 'NPM_TOKEN',
-  scopes: ['studio-75', 'ismyclub'],
-}
-
-const website = {
-  name: 'Website',
-  package: '@ismyclub/app.website',
-  command: 'website',
-}
-
-const cms = {
-  name: 'CMS',
-  package: '@ismyclub/app.cms',
-  command: 'cms',
-}
-
-const fairpark = {
-  name: 'Fairpark',
-  package: '@ismyclub/app.fairpark',
-  command: 'fairpark',
-}
-
-const signUp = {
-  name: 'Sign Up',
-  package: '@ismyclub/app.sign-up',
-  command: 'sign-up',
-}
-
-const standardConfig = {
-  LOG_LEVEL: 'info',
-};
-
-const pgConfig = {
-  PG_DATABASE: '',
-  PG_HOST: '',
-  PG_USER: '',
-  PG_PASSWORD: '',
-  PG_PORT: '',
-};
+import { cms, fairpark, infisical, npm, pgConfig, signUp, standardConfig, website } from './config';
 
 const run = async () => {
   await onboard().run();
