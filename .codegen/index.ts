b4e847402057177417780694012aba3bd7765f18
andrew robertson <andrewcrobertson@gmail.com>
2025-01-31 11:40:37 +1100

    bump-deps

--- .codegen/index.ts
+++ .codegen/index.ts
@@ -1,46 +1,31 @@
-import { appConfig, dockerNodeApp, dockerNodeAppConfig, infisicalConfig, npmConfig } from '@studio-75/sdk.container';
 import { onboard } from '@studio-75/sdk.onboard';
-import { cms, fairpark, infisical, npm, pgConfig, signUp, standardConfig, website } from './config';
+import { dockerfileNodeApp } from '@studio-75/sdk.dockerfile.node-app';
+import { dockerGitHubAction } from '@studio-75/sdk.github-action.docker.node-app';
+
+const envStandard = {
+  LOG_LEVEL: 'info',
+};
+
+const envDatabase = {
+  PG_DATABASE: '',
+  PG_HOST: '',
+  PG_USER: '',
+  PG_PASSWORD: '',
+  PG_PORT: '',
+};
+
+const infisical = { projectSlug: 'execution-environments-v5w-q', secretPath: '/github' };
+const npmSettings = { registry: 'https://npm.pkg.github.com/', scopes: ['studio-75', 'ismyclub'] };
+const env = { ...envStandard, ...envDatabase };
+const appWebsite = { name: 'Website', package: '@ismyclub/app.website', command: 'website', env };
+const appCms = { name: 'CMS', package: '@ismyclub/app.cms', command: 'cms', env };
+const appFairpark = { name: 'Fairpark', package: '@ismyclub/app.fairpark', command: 'fairpark', env };
+const appSignUp = { name: 'Sign Up', package: '@ismyclub/app.sign-up', command: 'sign-up', env };
 
 const run = async () => {
-  await onboard().run();
-
-  await dockerNodeApp()
-    .setInfisicalConfig(infisicalConfig().setProjectSlug(infisical.projectSlug).setSecretPath(infisical.secretPath).create())
-    .addNodeApp(
-      dockerNodeAppConfig()
-        .addNpmConfig(npmConfig().setRegistry(npm.registry).setTokenName(npm.tokenName).addScopes(npm.scopes).create())
-        .setAppConfig(appConfig().setName(cms.name).setPackage(cms.package).setCommand(cms.command).create())
-        .addEnvValues({ ...standardConfig, ...pgConfig })
-        .setExposePort(3000)
-        .create()
-    )
-    .addNodeApp(
-      dockerNodeAppConfig()
-        .addNpmConfig(npmConfig().setRegistry(npm.registry).setTokenName(npm.tokenName).addScopes(npm.scopes).create())
-        .setAppConfig(appConfig().setName(fairpark.name).setPackage(fairpark.package).setCommand(fairpark.command).create())
-        .addEnvValues({ ...standardConfig, ...pgConfig })
-        .setExposePort(3000)
-        .create()
-    )
-    .addNodeApp(
-      dockerNodeAppConfig()
-        .addNpmConfig(npmConfig().setRegistry(npm.registry).setTokenName(npm.tokenName).addScopes(npm.scopes).create())
-        .setAppConfig(appConfig().setName(signUp.name).setPackage(signUp.package).setCommand(signUp.command).create())
-        .addEnvValues({ ...standardConfig, ...pgConfig })
-        .setExposePort(3000)
-        .create()
-    )
-    .addNodeApp(
-      dockerNodeAppConfig()
-        .addNpmConfig(npmConfig().setRegistry(npm.registry).setTokenName(npm.tokenName).addScopes(npm.scopes).create())
-        .setAppConfig(appConfig().setName(website.name).setPackage(website.package).setCommand(website.command).create())
-        .addEnvValues(standardConfig)
-        .addEnvValues(pgConfig)
-        .setExposePort(3000)
-        .create()
-    )
-    .run();
+  await onboard();
+  await dockerfileNodeApp({ npmSettings, apps: [appWebsite, appCms, appFairpark, appSignUp] });
+  await dockerGitHubAction({ infisical, appNames: [appWebsite.name, appCms.name, appFairpark.name, appSignUp.name] });
 };
 
 run();
